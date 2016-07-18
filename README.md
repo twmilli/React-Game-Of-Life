0813b6f6826513bd55152b6178ddb2c4d72bc8ff
Taylor Milliman <twm013@bucknell.edu>
2016-07-18 15:52:34 -0400

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # React-Game-Of-Life
 Game of life as part of FCC curriculum
+http://twmilli.github.io/React-Game-Of-Life/
---
+++ app/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+require('./stylesheets/main.scss');
+var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>test</div>,
+  document.getElementById("app")
+);
